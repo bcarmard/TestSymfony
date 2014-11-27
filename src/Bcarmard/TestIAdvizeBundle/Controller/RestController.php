2efc8d57443b3eaf80ee7c02de98b5fac9fd1a6d
Bcarmard <bcarmard@gmail.com>
2014-11-27 14:08:45 +0100

    correction de l'affichage json afin qu'il soit plus propre + ajout des filtres par date et par auteur dans l'api

--- src/Bcarmard/TestIAdvizeBundle/Controller/RestController.php
+++ src/Bcarmard/TestIAdvizeBundle/Controller/RestController.php
@@ -1,31 +1,113 @@
 <?php
 
 namespace Bcarmard\TestIAdvizeBundle\Controller;
-use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\View\View,
-FOS\RestBundle\View\ViewHandler,
-FOS\RestBundle\View\RouteRedirectView;
-use JMS\Serializer\SerializationContext;
 
+use DateTime;
+use Doctrine\DBAL\Types\Type;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
 /**
  * Description of RestController
+ * Ceci est un controleur REST full, utulisant FosRestConroller
  *
- * @author Duah
+ * @author Bcarmard
+ * 
  */
 class RestController extends FOSRestController
 {
     
-    public function getPostsAction()
-    {
-             
-        $em = $this->getDoctrine()
-                ->getManager();
-        $repo = $em->getRepository('BcarmardTestIAdvizeBundle:ApiVdm');
-        $results = $repo->findAll();
+    
+    /**
+     * @QueryParam(name="from", requirements="^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$", default="1", description="Date de début.")
+     * @QueryParam(name="to", requirements="^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$", default="1", description="Date de fin.")
+     * @QueryParam(name="author", requirements="^\w+\s\w+$", default="1", description="Auteur")
+     * @param ParamFetcher $paramFetcher
+     */
+    public function getPostsAction(ParamFetcher $paramFetcher)
+    {   
+        //récupération des paramètres passés en Get
+        $from = $paramFetcher->get('from');
+        //var_dump($from);
+        $to = $paramFetcher->get('to');
+        //var_dump($to);
+        $author = $paramFetcher->get('author');
         
-        $view = $this->view($results, 200)
+        //appel du repo       
+        $em = $this->getDoctrine()
+                    ->getManager();
+        $repo = $em->getRepository('BcarmardTestIAdvizeBundle:ApiVdm');
+        
+        //si From et To sont définis
+        if($from != 1  && $to != 1 )
+        {
+            $date_from = new DateTime($from);
+            $date_to = new DateTime($to);
+            $qb = $em->createQueryBuilder();
+            $query = $qb->select('s')
+                        ->from('Bcarmard\TestIAdvizeBundle\Entity\ApiVdm', 's')
+                        ->andWhere($qb->expr()->between('s.date', ':date_from', ':date_to'))
+                        ->setParameter('date_from', $date_from, \Doctrine\DBAL\Types\Type::DATETIME)
+                        ->setParameter('date_to', $date_to, \Doctrine\DBAL\Types\Type::DATETIME)
+                        ->getQuery();
+           
+            $posts = $query->getResult();
+        }
+        //si juste From est défini
+        elseif($from != 1)
+        {
+            $date_from = new DateTime($from);
+            $qb = $em->createQueryBuilder();
+            $query = $qb->select('s')
+                        ->from('Bcarmard\TestIAdvizeBundle\Entity\ApiVdm', 's')
+                        ->where($qb->expr()->gte('s.date', ':date_from'))
+                        ->setParameter('date_from', $date_from, \Doctrine\DBAL\Types\Type::DATETIME)
+                        ->getQuery();
+           
+            $posts = $query->getResult();
+            
+        }
+        //si juste To est défini
+        elseif($to != 1)
+        {
+            $date_to = new DateTime($to);
+            $qb = $em->createQueryBuilder();
+            $query = $qb->select('s')
+                        ->from('Bcarmard\TestIAdvizeBundle\Entity\ApiVdm', 's')
+                        ->where($qb->expr()->lte('s.date', ':date_to'))
+                        ->setParameter('date_to', $date_to, \Doctrine\DBAL\Types\Type::DATETIME)
+                        ->getQuery();
+           
+            $posts = $query->getResult();
+        }
+        //si l'auteur est défini
+        elseif($author !=1)
+        {
+            $qb = $em->createQueryBuilder();
+            $query = $qb->select('s')
+                        ->from('Bcarmard\TestIAdvizeBundle\Entity\ApiVdm', 's')
+                        ->where($qb->expr()->eq('s.auteur', ':author'))
+                        ->setParameter('author', $author)
+                        ->getQuery();
+           
+            $posts = $query->getResult();
+        }
+        //dans tous les autres cas
+        else{
+            $posts = $repo->findAll();
+        }
+        
+        $count = count($posts);
+        
+        
+        $view = $this->view(array(
+                    'posts' => $posts,
+                    'count' => $count)
+                , 200)
                 ->setTemplate("TestIAdvizeBundle:ApiRest:get.html.twig")
-                ->setTemplateVar('posts');
+                ->setTemplateVar('posts','count');
+        
         
         return $this->handleView($view);
     }
@@ -36,11 +118,15 @@ class RestController extends FOSRestController
         $em = $this->getDoctrine()
                 ->getManager();
         $repo = $em->getRepository('BcarmardTestIAdvizeBundle:ApiVdm');
-        $results = $repo->find($id);
+        $post = $repo->find($id);
         
-        $view = $this->view($results, 200)
+        $view = $this->view(array(
+                    'post' => $post
+                )
+                , 200)
                 ->setTemplate("TestIAdvizeBundle:ApiRest:get.html.twig")
-                ->setTemplateVar('posts');
+                ->setTemplateVar('post');
+        
         
         return $this->handleView($view);
     }
